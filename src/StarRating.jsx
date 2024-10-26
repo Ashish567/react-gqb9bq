9f2418ef5ffdaf084e9450d7286201ba0fee8545
Ashish567 <djdaydreamer.singh4@gmail.com>
2024-10-26 20:36:23 +0530

    star rating completed

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -2,23 +2,19 @@ import React,{useEffect, useState} from 'react'
 import './StarRating.css'
 export default function StarRating() {
     const [fill, setFill] = useState(0);
-    const [clicked, setClicked] = useState(0)
-    useEffect(() => {
-        console.log(fill)
-    },[fill])
+    const [clicked, setClicked] = useState(-1)
+    
     return (
     <div className='rating-container'>
         <div>Star Rating...</div>
-        <div className="star-rating" onMouseLeave={() => {
-            setFill(-1)
-        }}>
+        <div className="star-rating" onMouseLeave={() => setFill(clicked)}>
             {
-                [1,2,3,4,5].map((val,index) => <Star key={index} val={val} setFill={setFill} fill={fill} />)
+                [1,2,3,4,5].map((val,index) => <Star key={index} val={val} setFill={setFill} fill={fill} setClicked={setClicked} />)
             }
         </div>
     </div>)
 }
-function Star({val, setFill, fill}) {
+function Star({val, setFill, fill, setClicked}) {
     const [color, setColor] = useState('gray');
     useEffect(() => val <= fill ? setColor('yellow') : setColor('gray'),[fill])
     function fillStar(val) {
@@ -26,7 +22,5 @@ function Star({val, setFill, fill}) {
     }
     return <span className="star" style={{
         color: color
-    }} onMouseEnter={() => fillStar(val)} onClick={() => {
-        console.log("star clicked! " + val)
-    }} data-value={val}>&#9733;</span>
+    }} onMouseEnter={() => fillStar(val)} onClick={() => setClicked(val)} data-value={val}>&#9733;</span>
 }
