bace45a64f1d535852434bd85140ffc826e60c4e
Ashish567 <djdaydreamer.singh4@gmail.com>
2024-10-26 21:07:58 +0530

    Tabs completed

---
+++ src/Tabs.jsx
@@ -0,0 +1,45 @@
+import React,{useState} from 'react'
+import './Tabs.css'
+
+export default function Tabs() {
+   const [tab, setTab] = useState([{
+      name:'Tab 1',
+      content: 'Content for Tab 1'
+   },{
+      name:'Tab 2',
+      content: 'Content for Tab 2'
+   },
+   {
+      name:'Tab 3',
+      content: 'Content for Tab 3'
+   }
+  ])
+   const [active,setActive] = useState('Tab 1')
+    return <>
+    <section class="tabs">
+       <ul class="tab-headers">
+         {
+            tab.map(({name}) => <li onClick={() => setActive(name)} class="tab-header">{name}</li>)
+         }
+       </ul>
+       <TabComp activeTab={active} tab={tab}/>
+    </section>
+    </>
+}
+
+function TabComp({activeTab, tab}) {
+ 
+ return (
+      <>
+        <div class="tab-contents">
+         {
+            tab.filter(({name}) => name === activeTab).map((data,index) => {
+               return <div key={index}>
+                  <div className='tab-content'>{data.content}</div>
+               </div>
+            })
+         }
+        </div>
+      </>
+ )
+}
